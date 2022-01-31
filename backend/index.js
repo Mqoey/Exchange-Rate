52dc6826968bf02ee69e9ed96580ce37c8e81649
Mqoey <mqoeyyy@gmail.com>
2022-01-31 17:16:56 +0200

    remove cors

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // create express app
 const app = express();
+
+// 
+app.use(cors());
 
 // setup the server port
 const port = process.env.PORT || 5000;
